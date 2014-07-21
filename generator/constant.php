62151a9defb04be973f961b2583d4403b26ac859
Hannes Magnusson <bjori@mongodb.com>
2014-07-21 12:44:05 -0700

    Use a define() for const values if possible

--- generator/constant.php
+++ generator/constant.php
@@ -1,5 +1,7 @@
 <?php if (is_numeric($cvalue)): ?>
 	zend_declare_class_constant_long(php_phongo_<?=strtolower($class)?>_ce, ZEND_STRL("<?=$cname?>"), 0x<?=sprintf("%02s", dechex($cvalue))?> TSRMLS_CC);
+<?php elseif (defined($cname)): ?>
+    zend_declare_class_constant_long(php_phongo_<?=strtolower($class)?>_ce, ZEND_STRL("<?=$cname?>"), <?=constant($cname)?> TSRMLS_CC);
 <?php else: ?>
 	zend_declare_class_constant_string(php_phongo_<?=strtolower($class)?>_ce, ZEND_STRL("<?=$cname?>"), "<?=$cvalue?>" TSRMLS_CC);
 <?php endif ?>
